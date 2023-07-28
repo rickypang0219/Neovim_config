27ce9451fbb94af92dc76cf740c9d36eb36fee3c
Ricky_Pang <60353958+rickypang0219@users.noreply.github.com>
2023-07-29 00:33:19 +0800

    uncomment hook for tsx jsx

--- lua/plugin_config/nvim-comment.lua
+++ lua/plugin_config/nvim-comment.lua
@@ -41,7 +41,7 @@ require('Comment').setup({
         extra = true,
     },
     ---Function to call before (un)comment
-    -- pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
     ---Function to call after (un)comment
     post_hook = nil,
 })
