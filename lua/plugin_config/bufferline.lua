2569323b121466ee09dbb1f96bb7a8290355a672
Ricky_Pang <60353958+rickypang0219@users.noreply.github.com>
2023-07-31 16:59:05 +0800

    add line to bufferline.lau for switching tabs

--- lua/plugin_config/bufferline.lua
+++ lua/plugin_config/bufferline.lua
@@ -1,2 +1,9 @@
 vim.opt.termguicolors = true
 require("bufferline").setup{}
+
+
+-- Switch tabs with arrow keys + Command (macOS)
+vim.cmd('nnoremap <M-,> :BufferLineCyclePrev<CR>')
+vim.cmd('nnoremap <M-.> :BufferLineCycleNext<CR>')
+vim.cmd('inoremap <M-,> <Esc>:BufferLineCyclePrev<CR>')
+vim.cmd('inoremap <M-.> <Esc>:BufferLineCycleNext<CR>')
