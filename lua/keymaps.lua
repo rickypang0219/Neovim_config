81c468a94a6d3567cc6641f6998e4c9a9ee97066
Ricky_Pang <60353958+rickypang0219@users.noreply.github.com>
2023-07-31 23:32:53 +0800

    modify indent keymapping
    Now using shift + > or < to indent or un-indent
    but now we do not need to reselect the code block again
    we can cont. pressing "shift + >/<" to indent

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,11 @@
 
 -- In Visual mode use Tab/ Shift + Tab for indent like VScode 
-vim.api.nvim_set_keymap('v', '<Tab>', '>gv', { noremap = true })
-vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true })
+-- vim.api.nvim_set_keymap('v', '<Tab>', '>gv', { noremap = true })
+-- vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true })
+--
+
+vim.api.nvim_set_keymap('v', '<S->>', '>gv', { noremap = true })
+vim.api.nvim_set_keymap('v', '<S-<>', '<gv', { noremap = true })
 
 
 --In Normal mode use Tab/ Shift + Tab for indent like VScode 
